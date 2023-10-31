aabdc27296248552e51583d1f9b02cd68830f9e4
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-10-31 08:26:06 -0300

    fix: refactoring basa_req_controller.py

--- src/controllers/baas/baas_req_controller.py
+++ src/controllers/baas/baas_req_controller.py
@@ -11,32 +11,23 @@ from src.errors.types.http_invalid_action import HttpInvalidActionError
 
 
 class BaasReqController(BaasReqInterface):
+    def __init__(self):
+        self.controllers = {
+            "create-person": CreatePersonController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/create-person'),
+            "login": LoginController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/login'),
+            "get-person": GetPersonController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/get-person'),
+            "deposit-saldo": DepositSaldoController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/deposit-saldo'),
+            "transfer-saldo": TransferSaldoController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/transfer-saldo'),
+            "withdraw-saldo": WithdrawSaldoController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/withdraw-saldo'),
+            "register-credit-card": RegisterCreditCardController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/register-credit-card'),
+            "create-payment": CreatePaymentController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/create-payment')
+        }
+
     def operate(self, data: dict) -> any:
         action = data.get("action")
+        controller = self.controllers.get(action)
 
-        if action == "create-person":
-            create_person_controller = CreatePersonController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/create-person')
-            return create_person_controller.operate(data)
-        elif action == 'login':
-            login_controller = LoginController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/login')
-            return login_controller.operate(data)
-        elif action == 'get-person':
-            get_person_controller = GetPersonController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/get-person')
-            return get_person_controller.operate(data)
-        elif action == 'deposit-saldo':
-            deposit_saldo_controller = DepositSaldoController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/deposit-saldo')
-            return deposit_saldo_controller.operate(data)
-        elif action == 'transfer-saldo':
-            transfer_saldo_controller = TransferSaldoController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/transfer-saldo')
-            return transfer_saldo_controller.operate(data)
-        elif action == 'withdraw-saldo':
-            withdraw_saldo_controller = WithdrawSaldoController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/withdraw-saldo')
-            return withdraw_saldo_controller.operate(data)
-        elif action == 'register-credit-card':
-            register_credit_card_controller = RegisterCreditCardController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/register-credit-card')
-            return register_credit_card_controller.operate(data)
-        elif action == 'create-payment':
-            create_payment_controller = CreatePaymentController('https://x0wiy4jqdf.execute-api.us-east-1.amazonaws.com/Prod/create-payment')
-            return create_payment_controller.operate(data)
+        if controller:
+            return controller.operate(data)
         else:
             raise HttpInvalidActionError("Action inválida!")
