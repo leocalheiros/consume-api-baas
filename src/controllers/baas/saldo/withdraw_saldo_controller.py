42afbeb6aef79c8387be80258cdaf9af7563cbd9
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-11-21 10:48:08 -0300

    fix: Refactoring baas saldo controllers to works with APIConsumer

--- src/controllers/baas/saldo/withdraw_saldo_controller.py
+++ src/controllers/baas/saldo/withdraw_saldo_controller.py
@@ -1,43 +1,36 @@
 import requests
 from flask import request
-from src.controllers.baas.interface.baas_controller_interface import BaasControllerInterface
+from src.controllers.interface.request_interface import RequestInterface
+from src.controllers.interface.api_consumer_interface import ApiConsumerInterface
 from src.errors.types.http_unprocessable_entity import HttpUnprocessableEntityError
-from src.errors.types.http_unauthorized import HttpUnauthorizedError
 
 
-class WithdrawSaldoController(BaasControllerInterface):
-    def __init__(self, api_url: str) -> None:
+class WithdrawSaldoController(RequestInterface):
+    def __init__(self, api_consumer: ApiConsumerInterface, api_method: str, api_url: str) -> None:
+        self.__api_consumer = api_consumer
+        self.api_method = api_method
         self.api_url = api_url
+        self.api_headers = {
+            "Content-type": "application/json",
+            "email": f"{request.headers.get('email')}",
+            "Authorization": f"Bearer {request.headers.get('Authorization').replace('Bearer ', '')}"
+        }
 
-    def operate(self, person_data: dict) -> any:
-        email = person_data.get("email")
-        amount = person_data.get("amount")
-        token = request.headers.get("Authorization").replace("Bearer ", "")
+    def send_request(self, request_data: dict) -> any:
+        email = request_data.get("email")
+        amount = request_data.get("amount")
+
         self.__validate(email)
         account_information = {
             "email": email,
             "amount": amount
         }
-        headers = {
-            "Authorization": f"Bearer {token}",
-            "email": email
-        }
 
-        response = requests.post(self.api_url, json=account_information, headers=headers)
-        return self.__format_response(response)
+        response = self.__api_consumer.request_response(self.api_method, self.api_url, self.api_headers,
+                                                        account_information)
+        return response
 
     def __validate(self, email: str):
         if email != request.headers.get("email"):
             raise HttpUnprocessableEntityError(
                 "O email da solicitação e dos headers devem ser iguais ao utilizado para gerar o token jwt")
-
-    def __format_response(self, response):
-        if response.status_code == 401:
-            error_response = response.json()
-            if "error" in error_response and error_response["error"] == "Token inválido!":
-                raise HttpUnauthorizedError("Token inválido!")
-        return self.__parse_api_response(response), response.status_code
-
-    def __parse_api_response(self, response) -> dict:
-        return response.json()
-
