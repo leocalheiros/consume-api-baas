27147935de72e32e3f9321e5bad47615b8e99401
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-11-21 10:49:51 -0300

    fix: Refactoring req_type_view

--- src/views/req_type_view.py
+++ src/views/req_type_view.py
@@ -1,20 +1,18 @@
-from src.views.http_types.http_request import HttpRequest
-from src.views.http_types.http_response import HttpResponse
 from src.views.interface.views_interface import ViewInterface
+from src.views.http_types.http_response import HttpResponse
+from src.views.http_types.http_request import HttpRequest
 from src.controllers.interface.type_controller_interface import TypeControllerInterface
-from src.errors.error_handler import handle_errors
 
 
 class ReqTypeView(ViewInterface):
-    def __init__(self, type_controller: TypeControllerInterface) -> None:
-        self.__type_controller = type_controller
+    def __init__(self, request_handler: TypeControllerInterface) -> None:
+        self.__request_handler = request_handler
 
     def handle(self, http_request: HttpRequest) -> HttpResponse:
-        try:
-            type_data = http_request.body
-            response, _ = self.__type_controller.operate(type_data)  # Ignorar o status code da nossa aplicação
-            status_code = response[1]  # Pegar o status code da API externa
-            response = response[0]  # Pegar a resposta da API externa
-            return HttpResponse(status_code, body=response)
-        except Exception as exception:
-            return handle_errors(exception)
+        request_data = http_request.body
+        response = self.__request_handler.operate(request_data)
+
+        return HttpResponse(
+            status_code=200,
+            body=response
+        )
